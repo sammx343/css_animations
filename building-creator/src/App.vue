ac3399d54fbf8bfc9548c737304c6e179c4d8225
Sebastian Mayor <proveedor636@culqi.com>
2025-09-09 00:20:52 -0500

    Fix click grid and change block functionality

--- building-creator/src/App.vue
+++ building-creator/src/App.vue
@@ -4,34 +4,46 @@
       <h2>Controls</h2>
       <div class="controls-container">
         <div class="cube-control" v-for="(block, index) in reactiveBlocks" :key="index">
-          <hr>
+          <hr />
           <h3>Block #{{ index + 1 }}</h3>
-          <hr>
+          <hr />
           <div class="expandable" :class="{ expanded: isBlockExpanded[index] }">
-            <CubeControls v-if="!isGridControlsOpen" :cube-properties="block"
-              @update:cube-properties="updateCubeProperties" @update:is-grid-control-open="updateIsGridControlOpen"
-              :zoom="zoom" />
-            <GridControls v-if="isGridControlsOpen" :selected-grid="selectedGrid"
-              :cube-dimensions="{ width: block.width, height: block.height }" :grid="block.grids"
-              @update:grids="updateGrid" @update:is-grid-control-open="updateIsGridControlOpen" />
+            <CubeControls
+              v-if="!isGridControlsOpen"
+              :cube-properties="block"
+              @update:cube-properties="updateCubeProperties"
+              @update:is-grid-control-open="(value) => updateIsGridControlOpen(value, block)"
+              :zoom="zoom"
+            />
           </div>
-          <p class="expandable-icon" @click="changeExpand(index)">V</p>
+          <div class="expandable-icon" @click="changeExpand(index)">
+            <v-icon v-if="isBlockExpanded[index]" name="bi-arrow-up" style="fill: black" />
+            <v-icon v-else name="bi-arrow-down" style="fill: black" />
+          </div>
         </div>
       </div>
+      <GridControls
+        v-if="isGridControlsOpen && selectedCube"
+        :selected-grid="selectedGrid"
+        :cube="selectedCube"
+        :grids="selectedCube.grids"
+        @update:grids="updateGrids"
+        @update:is-grid-control-open="updateIsGridControlOpen(false, null)"
+      />
     </div>
     <CubeScene :zoom="zoom" @update:zoom="updateZoom" :blocks="reactiveBlocks" />
   </main>
 </template>
 
 <script setup lang="ts">
-import { nextTick, reactive, ref, watch } from 'vue';
-import CubeScene from './components/CubeScene.vue';
-import CubeControls from './components/CubeControls.vue';
-import GridControls from './components/GridControls.vue';
-import type { Cube } from '@/types/cube';
-import type { Grid } from '@/types/grid';
-import { generateId } from './utils/generateId';
-import { useGridStore } from '@/store/gridStore';
+import { nextTick, reactive, ref, watch } from 'vue'
+import CubeScene from './components/CubeScene.vue'
+import CubeControls from './components/CubeControls.vue'
+import GridControls from './components/GridControls.vue'
+import type { Cube } from '@/types/cube'
+import type { Grid } from '@/types/grid'
+import { generateId } from './utils/generateId'
+import { useGridStore } from '@/store/gridStore'
 
 const blocks: Cube[] = [
   {
@@ -66,8 +78,8 @@ const blocks: Cube[] = [
         borderTop: {
           size: 0,
           style: 'none',
-          color: '#000000'
-        }
+          color: '#000000',
+        },
       },
     ],
   },
@@ -105,87 +117,65 @@ const blocks: Cube[] = [
         borderTop: {
           size: 0,
           style: 'none',
-          color: '#000000'
-        }
+          color: '#000000',
+        },
       },
     ],
   },
-];
+]
 
-const reactiveBlocks = ref<Cube[]>(blocks);
+const reactiveBlocks = ref<Cube[]>(blocks)
 
-const isBlockExpanded = ref(new Array(reactiveBlocks.value.length).fill(false));
+const isBlockExpanded = ref(new Array(reactiveBlocks.value.length).fill(false))
+const selectedGrid = ref<{ cubeId: string; grid: Grid }>()
+const gridStore = useGridStore()
+const selectedCube = ref<Cube>()
 
-const cubeProperties = reactive<Cube>({
-  id: generateId(),
-  width: 300,
-  height: 300,
-  long: 300,
-  colors: [{ hex: '#ff7360', percentage: 100 }],
-  colorsAngle: 0,
-  grids: [
-    {
-      id: generateId(),
-      name: 'Grid 0',
-      rows: 5,
-      columns: 5,
-      excludedWindows: [],
-      colors: [{ hex: '#000000', percentage: 100 }],
-      colorsAngle: 0,
-      top: '2%',
-      left: '2%',
-      windowWidth: '50%',
-      windowHeight: '50%',
-      gridWidth: '100%',
-      gridHeight: '100%',
-      rowGap: '5px',
-      columnGap: '5px',
-      borderRadius: '0%',
-      excludedFaces: [5, 6],
-      borderTop: {
-        size: 0,
-        style: 'none',
-        color: '#000000'
-      }
-    },
-  ],
-});
+const zoom = ref('1')
 
-const selectedGrid = ref<Grid>();
-const gridStore = useGridStore();
-
-const zoom = ref("1");
-
-const isGridControlsOpen = ref(false);
-
+const isGridControlsOpen = ref(false)
 
 const changeExpand = (index: number) => {
-  isBlockExpanded.value[index] = !isBlockExpanded.value[index];
-};
+  isBlockExpanded.value[index] = !isBlockExpanded.value[index]
+}
 
 watch(gridStore.getSelectedGrid, async (value) => {
-  isGridControlsOpen.value = true;
-  await nextTick();
-  selectedGrid.value = value;
-});
+  isGridControlsOpen.value = true
+  await nextTick()
+  selectedGrid.value = value?.grid
+  selectedCube.value = reactiveBlocks.value.find(
+    (reactiveBlock) => reactiveBlock.id === value?.cubeId,
+  )
+})
 
-const updateIsGridControlOpen = (value: boolean) => {
-  isGridControlsOpen.value = value;
-};
+const updateIsGridControlOpen = (value: boolean, cube: Cube | null) => {
+  isGridControlsOpen.value = value
+  if (cube) selectedCube.value = cube
+}
 
 const updateCubeProperties = (newBlock: Cube) => {
   reactiveBlocks.value = reactiveBlocks.value.map((block: Cube) => {
-    if (block.id === newBlock.id) return newBlock;
-    return block;
+    if (block.id === newBlock.id) return newBlock
+    return block
   })
-};
+}
 
-const updateGrid = (grids: Grid[]) => {
-  cubeProperties.grids = grids;
-};
+const updateGrids = (grids: Grid[], cubeId: string) => {
+  reactiveBlocks.value = reactiveBlocks.value.map((block: Cube) => {
+    if (block.id === cubeId) {
+      const newBlock = {
+        ...block,
+        grids,
+      }
+      selectedCube.value = newBlock
+      return newBlock
+    }
+    return block
+  })
+}
 
 const updateZoom = (value: string) => {
-  zoom.value = value;
+  zoom.value = value
 }
 </script>
 
